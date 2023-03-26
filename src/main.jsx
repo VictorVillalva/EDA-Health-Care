ff83ed0e4e24cba3cf360e2b9a493ac0d84d54ba
María José Domínguez Costa <majocosta4@gmail.com>
2023-03-26 11:55:26 -0600

    UpdatePatient al 99.9%

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import 'normalize.css';
+import PatientDetails from './pages/PatientDetails';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+    <PatientDetails />
+);
