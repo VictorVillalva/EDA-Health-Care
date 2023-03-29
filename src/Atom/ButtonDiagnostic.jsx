f204ea50bb58f973e0a59fceebb167f2f856c6ca
VictorVillalva <213380@ids.upchiapas.edu.mx>
2023-03-28 23:56:40 -0600

    Ya el ultimo

--- src/Atom/ButtonDiagnostic.jsx
+++ src/Atom/ButtonDiagnostic.jsx
@@ -1,14 +1,32 @@
 import '../assets/Styles/buttonDiagnostic.css';
+import { useState } from 'react';
+import Diagno from '../components/FormDiagnostic';
 
 const Button = () => {
+    const [diagnostic, setDiagnostic] = useState(false);
+    
+    const togglerDiagnostic = () =>{
+        setDiagnostic(!diagnostic);
+    }
+    if(diagnostic){
+        document.body.classList.add('active-diagnostic');
+    } else {
+        document.body.classList.remove('active-diagnostic');
+    }
     return (
         <>
             <div className="button-container">
                 <span className="button-text">Realizar Diagnostico</span>
-                <button className="base-btn">
+                <button onClick={togglerDiagnostic} className="base-btn">
                     <img src="src/assets/Images/buttonDiagnostic.png" className="diagnostic-btn" alt="Button Image"/>
                 </button>
             </div>
+            {diagnostic && (
+                <div className="newDiagnostic">
+                    <div onClick={togglerDiagnostic} className="overlay"></div>
+                    <Diagno />
+                </div>
+            )}
         </>
     )
 }
