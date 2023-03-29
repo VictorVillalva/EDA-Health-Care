18564230a258654a9fc6228be62af54fb79230fb
Eduardo Huerta <xsplited002@gmail.com>
2023-03-29 15:37:17 -0600

    added Contact options to FormSignUp

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -17,6 +17,10 @@ function FormSignUp(props) {
         telefono: '',
         email: '',
         contrasena: '',
+        nombreContacto: '',
+        apellidosContacto: '',
+        telefonoContacto: '',
+        emailContacto: '',
         tieneDoctor: false,
     });
 
@@ -172,6 +176,48 @@ function FormSignUp(props) {
                             id="contrasena"
                             name="contrasena"
                             value={formData.contrasena}
+                            onChange={handleInputChange}
+                            required
+                        />
+
+
+                        {/*<hr className="separator" />*/}
+                        <br/>
+                        <label htmlFor="contacto">CONTACTO:</label>
+                        <label htmlFor="nombre-contacto">Nombre:</label>
+                        <input
+                            type="text"
+                            id="nombre"
+                            name="nombre"
+                            value={formData.nombreContacto}
+                            onChange={handleInputChange}
+                            required
+                        />
+                        <label htmlFor="apellidos-contacto">Apellidos:</label>
+                        <input
+                            type="text"
+                            id="apellidos"
+                            name="apellidos"
+                            value={formData.apellidosContacto}
+                            onChange={handleInputChange}
+                            required
+                        />
+                        <label htmlFor="telefono-contacto">Número telefónico:</label>
+                        <input
+                            type="tel"
+                            id="telefono"
+                            name="telefono"
+                            value={formData.telefonoContacto}
+                            onChange={handleInputChange}
+                            required
+                        />
+
+                        <label htmlFor="email-contacto">Correo electrónico:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.emailContacto}
                             onChange={handleInputChange}
                             required
                         />
